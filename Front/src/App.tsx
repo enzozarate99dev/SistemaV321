d56da65373c7ed5ee807c9d9e32f5a022cd965cf
enzozarate99dev <enzozarateok66@gmail.com>
2023-01-13 15:50:00 -0300

    Actualizar App.tsx

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,34 +1,41 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import AutenticacionContext from './auth/AutenticacionContext';
-import { obtenerClaims } from './auth/handlerJWT';
-import './fondo.css';
-import rutas from './Generales/routesConfig';
-import { claim } from './Models/auth.model';
-import Menu from './utils/Menu';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import AutenticacionContext from "./auth/AutenticacionContext";
+import { obtenerClaims } from "./auth/handlerJWT";
+import "./fondo.css";
+import rutas from "./Generales/routesConfig";
+import { claim } from "./Models/auth.model";
+import Menu from "./utils/Menu";
 
 function App() {
-
-  const [claims, setClaims] = useState<claim[]>([])
+  //swdf
+  const [claims, setClaims] = useState<claim[]>([]);
 
   useEffect(() => {
-    setClaims(obtenerClaims())
-    console.log(claims)
-  }, [])
+    setClaims(obtenerClaims());
+    console.log(claims);
+  }, []);
 
   function actualizar(claims: claim[]) {
-    setClaims(claims)
+    setClaims(claims);
   }
 
   function esAdmin() {
-    return claims.findIndex(claim => claim.nombre === 'role' && claim.valor === 'admin') > -1;
+    return (
+      claims.findIndex(
+        (claim) => claim.nombre === "role" && claim.valor === "admin"
+      ) > -1
+    );
   }
 
   function esCajero() {
-    return claims.findIndex(claim => claim.nombre === 'role' && claim.valor === 'cajero') > -1;
+    return (
+      claims.findIndex(
+        (claim) => claim.nombre === "role" && claim.valor === "cajero"
+      ) > -1
+    );
   }
-
 
   return (
     <>
@@ -37,11 +44,21 @@ function App() {
           <Menu />
           <div className="container">
             <Switch>
-              {rutas.map(ruta =>
-                <Route key={ruta.path} path={ruta.path}
-                  exact={ruta.exact}>
-                  {ruta.esCajero && (!esCajero() && !esAdmin()) ? <>No tienes permiso</> : <>{ruta.esAdmin && !esAdmin() ? <>No tienes permiso</> : <ruta.componente/>}</>}                 
-                </Route>)}
+              {rutas.map((ruta) => (
+                <Route key={ruta.path} path={ruta.path} exact={ruta.exact}>
+                  {ruta.esCajero && !esCajero() && !esAdmin() ? (
+                    <>No tienes permiso</>
+                  ) : (
+                    <>
+                      {ruta.esAdmin && !esAdmin() ? (
+                        <>No tienes permiso</>
+                      ) : (
+                        <ruta.componente />
+                      )}
+                    </>
+                  )}
+                </Route>
+              ))}
             </Switch>
           </div>
         </AutenticacionContext.Provider>

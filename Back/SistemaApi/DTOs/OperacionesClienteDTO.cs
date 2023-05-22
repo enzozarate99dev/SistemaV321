ec717372b877a5e0509cfea8c97af49c855d268a
enzozarate99dev <enzozarateok66@gmail.com>
2023-05-22 11:02:57 -0300

    asd

--- Back/SistemaApi/DTOs/OperacionesClienteDTO.cs
+++ Back/SistemaApi/DTOs/OperacionesClienteDTO.cs
@@ -1,14 +1,20 @@
-﻿namespace SistemaApi.DTOs
+﻿using SistemaApi.Entidades;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaApi.DTOs
 {
     public class OperacionesClienteDTO
     {
         public int Id_venta { get; set; }
-        public int ClienteId { get; set; }
         public double PrecioTotal { get; set; }
         public DateTime FechaDeVenta { get; set; }
         public string TipoComprobante { get; set; }
         public bool Pagada { get; set; }
+        public int ClienteId { get; set; }
+        public ClienteDTO Cliente { get; set; }  
         public List<PagoDTO> Pagos { get; set; }
+        
+
 
     }
 }

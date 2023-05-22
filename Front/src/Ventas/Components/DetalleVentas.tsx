74cf160ccb26206f29838b0476f2db5b4eb008df
enzozarate99dev <enzozarateok66@gmail.com>
2023-05-22 11:13:27 -0300

    creacion

--- Front/src/Ventas/Components/DetalleVentas.tsx
+++ Front/src/Ventas/Components/DetalleVentas.tsx
@@ -1,31 +1,67 @@
-import { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { ventasModel } from "../../Models/ventas.model";
-import * as services from "../Services/ventas.services";
-import * as serClientes from "../../Clientes/Services/clientes.services";
-import { clienteModel } from "../../Models/clientes.model";
+import "./ventaStyles.css";
 
 export default function DetalleVentas(props: infoVentaProps) {
-  const venta = props.venta;
-  const ventaLine = venta.ventaLine;
-  const productos = ventaLine.map((v: any) => v.producto.nombre);
-  console.log(ventaLine);
+  const ventaLine = props.detalle.articulos;
+  const { cliente, domicilio, nroDocumento, email, condicion } = props.detalle.clienteInfor;
 
   return (
     <div className="container">
-      <h4 style={{ marginTop: "1rem" }}>Detalle de venta {props.venta.id_venta}</h4>
-      <div>
-        <p> Productos: {productos} </p>
-        <p> Precio del producto: {ventaLine[0].precioUnitario} </p>
-        <p> Cantidad comprada: {ventaLine[0].cantidad} </p>
-      </div>
+      {props.ocultarInfo ? (
+        <></>
+      ) : (
+        <div className="row m-0">
+          <div className="col">
+            <h5>Información del cliente </h5>
+            <div className="container">
+              <p>
+                <strong>CLIENTE:</strong> {cliente}
+              </p>
+              <p>
+                <strong>DIRECCION:</strong> {domicilio}
+              </p>
+              <p>
+                <strong>DNI:</strong> {nroDocumento}
+              </p>
+              <p>
+                <strong>EMAIL:</strong> {email}
+              </p>
+              <p>
+                <strong>CONDICION:</strong> {condicion}
+              </p>
+            </div>
+          </div>
+          <div className="col"></div>
+        </div>
+      )}
+      <h5> Artículos</h5>
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th>Codigo Artículo</th>
+            <th>Descripción</th>
+            <th>Cantidad comprada</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {ventaLine.map((vl: any) => (
+            <tr key={vl.id_venta_line}>
+              <td>{vl.producto.codigo}</td>
+              <td>
+                {vl.producto.nombre} - {vl.producto.descripcion}
+              </td>
+              <td>{vl.cantidad}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
 interface infoVentaProps {
-  venta: any;
-  setFlagModal: () => void;
-  setFlagListado: () => void;
+  ocultarInfo?: boolean;
+  detalle?: any;
+  setFlagModal?: () => void;
+  setFlagListado?: () => void;
 }

8cda06e75fb6ed2f89c1a17743c34d070a8f0bc3
enzozarate99dev <enzozarateok66@gmail.com>
2023-05-22 11:16:14 -0300

    cambios en los campos

--- Front/src/Productos/Components/FormularioProductos.tsx
+++ Front/src/Productos/Components/FormularioProductos.tsx
@@ -1,15 +1,13 @@
-import { Link } from "react-router-dom";
-import * as Yup from "yup";
-import useModal from "../../Compras/Components/useModal";
 import { productoCrear } from "../../Models/producto.model";
 // import Button from "../../utils/Button";
+import { Form, Input, Button, Upload } from "antd";
+// import { Form, Formik, FormikHelpers } from "formik";
+import { useState } from "react";
+import FormGroupText from "../../utils/FormGroupText";
 import FormGroupImagen from "../../utils/FormGroupImagen";
 import FormGroupMarkdown from "../../utils/FormGroupMarkdown";
-import FormGroupText from "../../utils/FormGroupText";
-import { Checkbox, Form, Input, Button } from "antd";
-import { useEffect, useState } from "react";
-import Upload from "antd/es/upload/Upload";
 import { PlusOutlined } from "@ant-design/icons";
+import { urlProductos } from "../../Generales/endpoints";
 // import { Form, Formik, FormikHelpers } from "formik";
 
 export default function FormularioProductos(props: formularioProductosProps) {
@@ -26,6 +24,7 @@ export default function FormularioProductos(props: formularioProductosProps) {
     ["cantidad"]: props.modelo.cantidad,
     // ["foto"]: props.modelo.foto,
   });
+  console.log(props.modelo.foto, "modelo");
   return (
     <Form
       form={form}
@@ -35,38 +34,41 @@ export default function FormularioProductos(props: formularioProductosProps) {
       wrapperCol={{ span: 16 }}
       style={{ marginTop: "-1px", backgroundColor: "#D9D9D9" }}
       initialValues={props.modelo}
-      onFinish={props.onSubmit}
+      // onFinish={props.onSubmit}
+      onFinish={(values) => {
+        props.onSubmit(values);
+      }}
       autoComplete="off"
     >
       <div className="col-md-4">
         <Form.Item name="nombre" rules={[{ required: true, message: "" }]}>
-          <Input placeholder="Nombre del producto" disabled={disabled} />
+          <Input placeholder="Nombre del producto" disabled={disabled} addonBefore="Producto" />
         </Form.Item>
       </div>
       <div className="col-md-4">
         <Form.Item name="precio" rules={[{ required: true, message: "" }]}>
-          <Input placeholder="Precio" disabled={disabled} />
-        </Form.Item>
-      </div>
-      <div className="col-md-4">
-        <Form.Item name="descripcion" rules={[{ required: true, message: "" }]}>
-          <Input.TextArea placeholder="Descripción" disabled={disabled} />
+          <Input placeholder="Precio" disabled={disabled} addonBefore="Precio" />
         </Form.Item>
       </div>
 
       <div className="col-md-4">
         <Form.Item name="categoria" rules={[{ required: true, message: "" }]}>
-          <Input placeholder="Categoría" disabled={disabled} />
+          <Input placeholder="Categoría" disabled={disabled} addonBefore="Categoria" />
         </Form.Item>
       </div>
       <div className="col-md-4">
         <Form.Item name="codigo" rules={[{ required: true, message: "" }]}>
-          <Input placeholder="Código" disabled={disabled} />
+          <Input placeholder="Código" disabled={disabled} addonBefore="Codigo" />
         </Form.Item>
       </div>
       <div className="col-md-4">
         <Form.Item name="cantidad" rules={[{ required: true, message: "" }]}>
-          <Input placeholder="Cantidad" />
+          <Input placeholder="Cantidad" addonBefore="Cantidad" />
+        </Form.Item>
+      </div>
+      <div className="col-md-4">
+        <Form.Item name="descripcion" rules={[{ required: true, message: "" }]}>
+          <Input.TextArea placeholder="Descripción" disabled={disabled} />
         </Form.Item>
       </div>
       <div
@@ -77,21 +79,18 @@ export default function FormularioProductos(props: formularioProductosProps) {
           marginInline: "20vw",
         }}
       >
-        {/* <div className="col-md-7">
+        <div className="col-md-7">
           <div className="col-md-4">
-            <Form.Item name="foto">
-              <FormGroupImagen campo="foto" label="Foto" imagenURL={props.modelo.fotoURL} />
-            </Form.Item>
-             <Form.Item label="" valuePropName="fileList">
-              <Upload action="/upload.do" listType="picture-card">
+            <Form.Item label="" name="foto">
+              <Upload listType="picture-card">
                 <div>
                   <PlusOutlined />
                   <div style={{ marginTop: 8 }}>Foto</div>
                 </div>
               </Upload>
-            </Form.Item> 
+            </Form.Item>
           </div>
-        </div> */}
+        </div>
       </div>
       {props.buttonExiste ? (
         <div className="container" style={{ display: "flex", justifyContent: "center" }}>
@@ -101,6 +100,61 @@ export default function FormularioProductos(props: formularioProductosProps) {
         </div>
       ) : null}
     </Form>
+    // <Formik initialValues={props.modelo} onSubmit={props.onSubmit}>
+    //   {(formikProps) => (
+    //     <Form style={{ marginTop: "-10px" }} className="row g-3 needs-validation" noValidate>
+    //       <div className="col-md-4">
+    //         <FormGroupText campo="nombre" label="Nombre" placeholder="Nombre del producto" />
+    //       </div>
+    //       <div className="col-md-4">
+    //         <div className="input-group has-validation">
+    //           <span style={{ height: "38px", marginTop: "32px" }} className="input-group-text" id="inputGroupPrepend">
+    //             $
+    //           </span>
+    //           <FormGroupText campo="precio" label="Precio" placeholder="Precio del producto ($)" />
+    //         </div>
+    //       </div>
+    //       <div className="col-md-4">
+    //         <FormGroupText campo="cantidad" label="Cantidad disponible" placeholder="Cantidad disponible" />
+    //       </div>
+    //       <div className="col-md-6">
+    //         <FormGroupText campo="codigo" label="Codigo" placeholder="Codigo" />
+    //       </div>
+    //       <div className="col-md-6">
+    //         <FormGroupText campo="categoria" label="Categoria" placeholder="Categoria" />
+    //       </div>
+    //       <div className="col-md-8">
+    //         <FormGroupImagen campo="foto" label="Foto" imagenURL={props.modelo.fotoURL} />
+    //       </div>
+    //       <div className="col-md-10">
+    //         <FormGroupMarkdown campo="descripcion" label="Descripcion" />
+    //       </div>
+    //       <div className="col-12">
+    //         <Button disabled={formikProps.isSubmitting} type="submit">
+    //           Guardar
+    //         </Button>
+    //         <Button
+    //           className="btn btn-danger"
+    //           style={{ marginLeft: "0.5rem" }}
+    //           onClick={() => {
+    //             formikProps.setValues(props.modelo);
+    //           }}
+    //         >
+    //           Limpiar
+    //         </Button>
+    //         <Button
+    //           className="btn btn-secondary"
+    //           style={{ marginLeft: "0.5rem" }}
+    //           onClick={() => {
+    //             props.setBandera!();
+    //           }}
+    //         >
+    //           Salir
+    //         </Button>
+    //       </div>
+    //     </Form>
+    //   )}
+    // </Formik>
   );
 }
 

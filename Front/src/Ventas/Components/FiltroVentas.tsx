40fee0f42c9a180f60706f5d16c9c1d0c51c3aab
enzozarate99dev <enzozarateok66@gmail.com>
2023-05-22 11:13:10 -0300

    linter

--- Front/src/Ventas/Components/FiltroVentas.tsx
+++ Front/src/Ventas/Components/FiltroVentas.tsx
@@ -1,76 +1,24 @@
-import axios, { AxiosResponse } from "axios";
-import { Field, Form, Formik } from "formik";
+import { AxiosResponse } from "axios";
+import { Form, Formik } from "formik";
 import { useEffect, useState } from "react";
-import { useHistory, useLocation } from "react-router-dom";
-import FilterIcon from "../../assets/FilterIcon";
-import { productoModel } from "../../Models/producto.model";
-import { ventasModel, ventasPostGetModel } from "../../Models/ventas.model";
-import { ventasConsumidorFinalModel } from "../../Models/ventasCf.model";
-import Button from "../../utils/Button";
-import FormGroupFecha from "../../utils/FormGroupFecha";
+import { useLocation } from "react-router-dom";
+import { ventasModel } from "../../Models/ventas.model";
 import Paginacion from "../../utils/Paginacion";
-import * as servicesCF from "../../VentasConsFinal/Services/consumidorFinal.services";
 import * as services from "../Services/ventas.services";
-import GenerarVentas from "./GenerarVentas";
-import { urlProductos } from "../../Generales/endpoints";
-import FormGroupText from "../../utils/FormGroupText";
-import * as servicesProd from "../../Productos/Services/productos.services";
+import * as servicesCliente from "../../Clientes/Services/clientes.services";
 import ListadoVentas from "./ListadoVentas";
 
 export default function FiltroVentas() {
   const [totalDePaginas, setTotalDePaginas] = useState(0);
-  const [productos, setProductos] = useState<productoModel[]>([]);
   const [ventas, setVentas] = useState<ventasModel[]>([]);
-  const [ventasCF, setVentasCF] = useState<ventasConsumidorFinalModel[]>([]);
-  const [mostrarFiltros, setMostrarFiltros] = useState(true);
   const [flag, setFlag] = useState(false);
 
   const handleFlag = () => {
     setFlag(!flag);
     console.log(flag);
   };
-  const history = useHistory();
 
   const query = new URLSearchParams(useLocation().search);
-
-  // const valorInicial: filtroProductosProps = {
-  //   nombre: "",
-  //   precio: 0,
-  //   codigo: "",
-  //   stockDisponible: false,
-  //   sinStock: false,
-  //   pagina: 1,
-  //   recordsPorPagina: 5,
-  // };
-
-  // useEffect(() => {
-  //   if (query.get("nombre")) {
-  //     valorInicial.nombre = query.get("nombre")!;
-  //   }
-  //   if (query.get("pagina")) {
-  //     valorInicial.pagina = parseInt(query.get("pagina")!, 5);
-  //   }
-  // });
-
-  // function modificarURL(valores: filtroProductosProps) {
-  //   const queryStrings: string[] = [];
-  //   if (valores.nombre) {
-  //     queryStrings.push(`nombre=${valores.nombre}`);
-  //   }
-  //   queryStrings.push(`pagina=${valores.pagina}`);
-  //   history.push(`/listadoVentas?${queryStrings.join("&")}`);
-  // }
-
-  // const buscarProducto = async (valores: filtroProductosProps) => {
-  //   modificarURL(valores);
-  //   const response = services.filtrar(valores);
-  //   await response.then((respuesta: AxiosResponse<productoModel[]>) => {
-  //     const totalDeRegistros = parseInt(respuesta.headers["cantidadtotalregistros"], 10);
-  //     setTotalDePaginas(Math.ceil(totalDeRegistros / valorInicial.recordsPorPagina));
-  //     setProductos(respuesta.data);
-  //   });
-  // };
-  // console.log(productos, "productos buscador");
 
   const valorInicial: filtroVentasProps = {
     productoId: 0,
@@ -84,49 +32,14 @@ export default function FiltroVentas() {
     sinStock: false,
   };
 
-  // useEffect(() => {
-  //   const res = axios.get(`${urlProductos}`);
-  //   res.then((respuesta: AxiosResponse<productoModel[]>) => {
-  //     setProductos(respuesta.data);
-  //   });
-  // }, []);
-
   useEffect(() => {
-    // if (query.get("productoId")) {
-    //   valorInicial.productoId = parseInt(query.get("productoId")!, 10);
-    // }
-    // if (query.get("registrado")) {
-    //   valorInicial.registrado = true;
-    // }
-    // if (query.get("consumidor")) {
-    //   valorInicial.consumidor = true;
-    // }
     if (query.get("pagina")) {
       valorInicial.pagina = parseInt(query.get("pagina")!, 10);
     }
     buscarVenta(valorInicial);
   }, [flag]);
 
-  function modificarURL(valores: filtroVentasProps) {
-    const queryStrings: string[] = [];
-    // if (valores.productoId) {
-    //   queryStrings.push(`productoId=${valores.productoId}`);
-    // }
-    // if (valores.fechaDeVenta) {
-    //   queryStrings.push(`fechaDeVenta=${valores.fechaDeVenta}`);
-    // }
-    // if (valores.registrado) {
-    //   queryStrings.push(`registrado=${valores.registrado}`);
-    // }
-    // if (valores.consumidor) {
-    //   queryStrings.push(`consumidor=${valores.consumidor}`);
-    // }
-    queryStrings.push(`pagina=${valores.pagina}`);
-    history.push(`/listadoVentas?${queryStrings.join("&")}`);
-  }
-
   async function buscarVenta(valores: filtroVentasProps) {
-    // modificarURL(valores);
     const res = services.filtrar(valores);
     res.then((respuesta: AxiosResponse<ventasModel[]>) => {
       const totalDeRegistros = parseInt(respuesta.headers["cantidadtotalregistros"], 10);
@@ -134,45 +47,9 @@ export default function FiltroVentas() {
       setVentas(respuesta.data);
     });
   }
-  console.log(ventas);
 
   return (
     <>
-      {/* <Formik
-        initialValues={valorInicial}
-        onSubmit={(valores) => {
-          valores.pagina = 1;
-          buscarProducto(valores);
-        }}
-      >
-        {(formikProps) => (
-          <>
-            <GenerarVentas
-              ventas={ventas}
-              ventasConsFinal={ventasCF}
-              setFlag={handleFlag}
-              productos={productos}
-              buscador={
-                <Form>
-                  <div className="form-inline">
-                    <FormGroupText onChange={() => formikProps.submitForm()} campo="nombre" placeholder="Cargar articulo" />
-                  </div>
-                </Form>
-              }
-              paginacion={
-                <Paginacion
-                  cantidadTotalDePaginas={totalDePaginas}
-                  paginaActual={formikProps.values.pagina}
-                  onChange={(nuevaPagina) => {
-                    formikProps.values.pagina = nuevaPagina;
-                    buscarProducto(formikProps.values);
-                  }}
-                />
-              }
-            />
-          </>
-        )}
-      </Formik> */}
       <Formik
         initialValues={valorInicial}
         onSubmit={(valores) => {
@@ -183,9 +60,7 @@ export default function FiltroVentas() {
         {(formikProps) => (
           <>
             <Form>
-              <div className="form-inline">
-                {/* <FormGroupText onChange={() => formikProps.submitForm()} campo="nombre" placeholder="Buscar venta" /> */}
-              </div>
+              <div className="form-inline"></div>
             </Form>
 
             <ListadoVentas ventas={ventas} setFlag={handleFlag}></ListadoVentas>

e5efb92d9779893aa6898840a55c3d954d52cbab
enzozarate99dev <enzozarateok66@gmail.com>
2023-06-01 16:47:26 -0300

    exportar sucursalId del token

--- Front/src/auth/handlerJWT.ts
+++ Front/src/auth/handlerJWT.ts
@@ -1,35 +1,37 @@
 import { claim, respuestaAutenticacion } from "../Models/auth.model";
+const llaveToken = "token";
+const llaveExpiracion = "token-expiracion";
+const claveSecreta = "llavejwt";
 
-const llaveToken = "token"
-const llaveExpiracion = "token-expiracion"
-
-export function guardarTokenLocalStorage(autenticacion: respuestaAutenticacion){
-    localStorage.setItem(llaveToken, autenticacion.token)
-    localStorage.setItem(llaveExpiracion, autenticacion.expiracion.toString())
+export function guardarTokenLocalStorage(autenticacion: respuestaAutenticacion) {
+  localStorage.setItem(llaveToken, autenticacion.token);
+  localStorage.setItem(llaveExpiracion, autenticacion.expiracion.toString());
 }
 
-export function obtenerClaims():claim[]{
-    const token = localStorage.getItem(llaveToken)
-    if(!token){
-        return []
-    }
-    const expiracion = localStorage.getItem(llaveExpiracion)!
-    const expiracionFecha = new Date(expiracion)
+export function obtenerClaims(): claim[] {
+  const token = localStorage.getItem(llaveToken);
+  if (!token) {
+    return [];
+  }
+  const expiracion = localStorage.getItem(llaveExpiracion)!;
+  const expiracionFecha = new Date(expiracion);
 
-    if(expiracionFecha<= new Date()){
-        logout()
-        return []
-    }
+  if (expiracionFecha <= new Date()) {
+    logout();
+    return [];
+  }
 
-    const dataToken = JSON.parse(window.atob(token.split(".")[1]))
-    const respuesta: claim[] = []
-    for(const propiedad in dataToken){
-        respuesta.push({nombre: propiedad, valor: dataToken[propiedad]})
-    }
-    return respuesta
+  const dataToken = JSON.parse(window.atob(token.split(".")[1]));
+
+  const respuesta: claim[] = [];
+  for (const propiedad in dataToken) {
+    respuesta.push({ nombre: propiedad, valor: dataToken[propiedad] });
+  }
+  return respuesta;
 }
 
-export function logout(){
-    localStorage.removeItem(llaveToken)
-    localStorage.removeItem(llaveExpiracion)
+export function logout() {
+  localStorage.removeItem(llaveToken);
+  localStorage.removeItem(llaveExpiracion);
 }
+export const sucID = obtenerClaims().find((claim) => claim.nombre === "sucursalId")?.valor;
